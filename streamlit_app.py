ece00585dc79e71d29ba86e5cd36dcc660774751
walid ahmed <47761160+walidcol@users.noreply.github.com>
2022-08-29 13:27:37 -0400

    Update streamlit_app.py
    added images and one new item and changed header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Breakfast Favorites')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
